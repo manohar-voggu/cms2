69fe3da281330227ac4f0f04b889d6d8da6fd5f0
Manohar Reddy <f20180564@hyderabad.bits-pilani.ac.in>
2020-04-24 16:20:45 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CMS());
+}
+
+class CMS extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
