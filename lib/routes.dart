5ffc56a9594ae91dbbd879bc4afcbcedb7c1e0bc
Manohar Reddy <f20180564@hyderabad.bits-pilani.ac.in>
2020-04-28 11:36:16 +0530

    Each screen has a folder

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
-import 'package:cms_flutter/screens/sections.dart';
-import 'package:cms_flutter/screens/courses.dart';
-import 'package:cms_flutter/screens/login.dart';
+import 'package:cms_flutter/screens/sections/sections.dart';
+import 'package:cms_flutter/screens/courses/courses.dart';
+import 'package:cms_flutter/screens/login/login.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   '/login': (BuildContext context) => Login(),
